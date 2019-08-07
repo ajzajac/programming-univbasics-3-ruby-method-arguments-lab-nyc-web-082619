57eb738cb7ce3e85e88e90bfdd9351e26f52fd77
Andrew Zajac <ajzajac@icloud.com>
2019-08-07 15:39:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -83,7 +83,7 @@
 
 
 
-# def introduction(name)
+ def introduction(name)
 #  puts "Hi, my name is #{name}."
 #end
 
@@ -93,4 +93,4 @@
 
 #def introduction_with_language_optional(name, language="Ruby")
 #  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+#end#
